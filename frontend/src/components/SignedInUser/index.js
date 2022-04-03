430d601d9a301752a7e3a929a793b6c029e4655b
YinYang117 <slcryan2010@yahoo.com>
2022-04-03 13:34:32 -0600

    changes to login and signup modals

--- frontend/src/components/SignedInUser/index.js
+++ frontend/src/components/SignedInUser/index.js
@@ -1,13 +1,14 @@
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 
 const SignedInUser = () => {
-
+  const sessionUser = useSelector(state => state.session.user)
 
   return (
-    <>
+  <div className="signed-in-container">
     <div>Welcome {sessionUser.username}</div>
     <ProfileButton user={sessionUser} />
-  </>
+  </div>
   );
 };
 
--- frontend/src/components/WelcomeModal/SignupForm.js
+++ frontend/src/components/WelcomeModal/SignupForm.js
@@ -10,6 +10,8 @@ function SignupForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [securityQuestion, setSecurityQuestion] = useState("");
+  const [securityQuestionAnswer, setSecurityQuestionAnswer] = useState("");
   const [errors, setErrors] = useState([]);
 
   if (sessionUser) return <Redirect to="/" />;
@@ -28,48 +30,84 @@ function SignupForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
+    <div className="signup-modal" >
+      <h3 className="welcome-div" >Welcome new friend!</h3>
+      <div className="welcome-div" >To the ultimate site for discovering new game worlds</div>
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <div className="labels-inputs">
+          <label>Email</label>
+          <input
+              className="form-input"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+          />
+        </div>
+        <div className="labels-inputs">
+          <label>Username</label>
+          <input
+            className="form-input"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+              required
+          />
+        </div>
+        <div className="labels-inputs">
+          <label>Password</label>
+          <input
+          className="form-input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div className="labels-inputs">
+          <label>Confirm Password</label>
+          <input
+          className="form-input"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div className="sec-qs">
+          <label>Your Custom Security Question</label>
+        </div>
+        <div className="sec-qs">
         <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
+          className="form-input"
+            type="security-question"
+            value={securityQuestion}
+            onChange={(e) => setSecurityQuestion(e.target.value)}
+            placeholder="Fav Color / High School"
+          />
+        </div>
+        <div className="sec-qs">
+          <label>Security Question Answer</label>
+        </div>
+        <div className="sec-qs">
         <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+          className="form-input"
+            type="security-question-answer"
+            value={securityQuestionAnswer}
+            onChange={(e) => setSecurityQuestionAnswer(e.target.value)}
+            placeholder="CaSe SeNsiTive Answer"
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+        <ul className="errors-list">
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+      </form>
+    </div>
   );
 }
 
